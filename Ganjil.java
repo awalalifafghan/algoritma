d08dd1f5e231268d2a2caed9d260ff810a7e0a10
awalalifafghan <146721128+awalalifafghan@users.noreply.github.com>
2023-10-02 17:13:05 +0700

    Add files via upload

---
+++ Ganjil.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ganjil {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner inputan = new Scanner(System.in);
+        System.out.print("Masukkan angka:");
+        int angka = inputan.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Bilangan Genap");
+        } else {
+            System.out.println("Bilangan Ganjil");
+            inputan.close();
+        }      
+    }
+}         
---
+++ Nilai.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Nilai{
+    
+    public static void main(String[] args) throws Exception {
+
+        Double nilai;
+        String indeksNilai;
+
+        Scanner inputan = new Scanner(System.in);
+        System.out.print("Masukkan nilai");
+        nilai = inputan.nextDouble();
+
+        if (nilai >= 75) {
+            indeksNilai ="A";
+        } else if (nilai >= 65) {
+            indeksNilai ="B";
+        } else if (nilai >=55) {
+            indeksNilai ="C";
+        } else {
+            indeksNilai = "E";
+        }
+
+        System.out.print("Indeks nilai:" + indeksNilai);
+        inputan.close();
+    }
+}
+
+
